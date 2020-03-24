88c5cafe65e6446eb7f080da3680726f7c4f78e4
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-03-24 02:27:11 -0500

    --- Auto-Git Commit ---

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,3 +1,8 @@
+import { createStore } from "redux"
+import testReducer from "../../features/testarea/testReducer";
+
 export const configureStore = () => {
-    const
+    const store = createStore(testReducer)
+
+    return store;
 }
--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+const mapState = state => ({
+  data: state.data
+});
 
 class TestComponent extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Test Component</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Test Component</h1>
+        <h3>The answer is: {this.props.data}</h3>
+      </div>
+    );
+  }
 }
 
-export default TestComponent
+export default connect(mapState)(TestComponent);
