f445eea2029e70b37f7b1a1066d45f2feb6ddae6
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-03-24 18:11:48 -0500

    --- Auto-Git Commit ---

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -22,7 +22,7 @@ class App extends Component {
             <Fragment>
               <NavBar />
               <Container className="main">
-                <Route path="/events" component={EventDashboard} />
+                <Route exact path="/events" component={EventDashboard} />
                 <Route path="/events/:id" component={EventDetailedPage} />
                 <Route path="/people" component={PeopleDashboard} />
                 <Route path="/profile/:id" component={UserDetailedPage} />
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
+import { Link } from "react-router-dom";
 
 class EventListItem extends Component {
   render() {
@@ -42,8 +43,8 @@ class EventListItem extends Component {
             content="Delete"
           />
           <Button
-            onClick={() => selectEvent(event)}
-            as="a"
+            as={Link}
+            to={'/events/${event.id}'}
             color="teal"
             floated="right"
             content="View"
