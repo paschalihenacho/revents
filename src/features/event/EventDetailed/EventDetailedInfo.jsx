04162752ace99bfde0e1596463c28bcb93bef7c2
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-04-04 00:48:27 -0500

    --- Auto-Git Commit ---

--- src/features/event/EventDetailed/EventDetailedInfo.jsx
+++ src/features/event/EventDetailed/EventDetailedInfo.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Segment, Grid, Icon, Button } from "semantic-ui-react";
+import EventDetailedMap from "./EventDetailedMap";
 
 const EventDetailedInfo = ({ event }) => {
+  const [isMapOpen, showMaptoggle] = useState(false);
   return (
     <Segment.Group>
       <Segment attached="top">
@@ -33,10 +35,21 @@ const EventDetailedInfo = ({ event }) => {
             <span>{event.venue}</span>
           </Grid.Column>
           <Grid.Column width={4}>
-            <Button color="teal" size="tiny" content="Show Map" />
+            <Button
+              onClick={() => showMaptoggle(!isMapOpen)}
+              color="teal"
+              size="tiny"
+              content="Show Map"
+            />
           </Grid.Column>
         </Grid>
       </Segment>
+      {isMapOpen && (
+        <EventDetailedMap
+          lat={event.venueLatLng.lat}
+          lng={event.venueLatLng.lng}
+        />
+      )}
     </Segment.Group>
   );
 };
--- src/features/event/EventDetailed/EventDetailedMap.jsx
+++ src/features/event/EventDetailed/EventDetailedMap.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Segment, Icon } from 'semantic-ui-react';
+import GoogleMapReact from "google-map-react";
+
+const Marker = () => <Icon name="marker" size="big" color="red" />;
 
 const EventDetailedMap = ({ lat, lng }) => {
     const zoom = 14;
     return (
-        <div>
-            
-        </div>
+        <Segment attached='bottom'>
+            <div style={{ height: "350px", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyBbHotqLrTYF8Gr61DAuptaX-KH_Y1jbYk" }}
+          defaultCenter={{lat, lng}}
+          defaultZoom={zoom}
+        >
+          <Marker lat={lat} lng={lng} />
+        </GoogleMapReact>
+      </div>
+</Segment>
     )
 }
 
